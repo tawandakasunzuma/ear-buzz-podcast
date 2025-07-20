b464bcb583d10c0052b2a4930d11e3aea3b80225
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-21 01:05:15 +0200

    create logic for audio player

---
+++ hooks/useAudio.js
@@ -0,0 +1,38 @@
+import { useState, useEffect, useRef } from 'react';
+
+export default function useAudio () {
+
+    // Create state for audio link
+    const [audioSrc,setAudioSrc] = useState(null);
+    
+    // Create one audio player and keep it between rerenders
+    const audioRef = useRef (new Audio());
+
+    useEffect(() => {
+
+        // Fetch audio from API
+        fetch('https://podcast-api.netlify.app/audio/1')
+        
+            .then (response => {
+                // If response is not okay, throw an error
+                if (response.ok) throw new Error ("Audio could not be fetched");
+                // Parse the JSON response
+                return response.json();
+            })
+
+            .then (data => {
+                // Assign audio url to state
+                setAudioSrc(data.url);
+                // Set audio url as source of audio player
+                audioRef.current.src = data.url;
+            })
+
+            // Log any error that occurred during fetching
+            .catch((error) => {
+                console.error('Error: ',error);
+            })
+    },[])
+
+    // Return audio reference for later use
+    return audioRef
+}
--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import '../styles/AudioPlayer.css';
 import podcastIcon from '../assets/images/podcast-icon.svg'
 import playIcon from '../assets/images/play-icon.svg';
@@ -6,27 +6,45 @@ import altPlayIcon from '../assets/images/alt-play-icon.svg';
 import pauseIcon from '../assets/images/pause-icon.svg';
 import fastForward from '../assets/images/fast-forward-icon.svg';
 import fastRewind from '../assets/images/fast-rewind-icon.svg';
+import useAudio from '../../hooks/useAudio';
 
 export default function AudioPlayer () {
 
+    // Get reference to audio element using custom hook
+    const audioRef = useAudio();
+
+    // State to track where audio is playing or not
     const [isPlaying,setIsPlaying] = useState (false);
 
-    function handleChange () {
-        setIsPlaying(prev => !prev);
-    };
+    function togglePlay () {
+        // If audio source is not set, do nothing
+        if (!audioRef.current.src) {
+            return;
+        }
+        // Pause audio if it's playing, otherwise play ir
+        if (isPlaying) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.play();
+        }
+        // Update playing state on button clicks
+        setIsPlaying(prev => !prev)
+    }
     
     return (
         <div className='audio-player-section'>
+            {/* Left side of audio player */}
             <div className='left-side-audio-section'>
                 <img src={podcastIcon} alt="podcast icon" />
                 <p className='completion-status'>Completed</p>
             </div>
+            {/* Middle of audio player */}
             <div className='play-buttons-container'>
                 <button className='fast-forward-and-fast-rewind-btn'>
                     <img src={fastRewind} alt="fast rewind icon" />
                 </button>
                 <button 
-                    onClick={handleChange}
+                    onClick={togglePlay}
                     className='pause-and-play-btn'>
                         <img 
                             src={isPlaying ? pauseIcon : altPlayIcon} 
@@ -37,6 +55,7 @@ export default function AudioPlayer () {
                     <img src={fastForward} alt="fast forward icon" />
                 </button>
             </div>
+            {/* Right side of audio player */}
             <div className='right-side-audio-section'>
                 <input type="range" className='progress-bar' />
             </div>
